8dfeabc65c07e438f55a98d5319e50eafb33ab8e
flaviomoceri <moceriflavio@gmail.com>
2022-02-06 18:03:52 +0100

    spotify buit 2.0

---
+++ components/Player.js
@@ -0,0 +1,138 @@
+//import {
+//   HeartIcon, VolumeUpIcon as VolumeUpIcon
+//} from '@heroicons/react/outline';
+import { VolumeOffIcon } from '@heroicons/react/outline'
+import {
+  FastForwardIcon,
+  PauseIcon,
+  PlayIcon,
+  ReplyIcon,
+  RewindIcon,
+  VolumeUpIcon,
+  SwitchHorizontalIcon,
+} from '@heroicons/react/solid'
+import { debounce } from 'lodash'
+import { useSession } from 'next-auth/react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil'
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom'
+import useSongInfo from '../hooks/useSongInfo'
+import useSpotify from '../hooks/useSpotify'
+
+function Player() {
+  const spotifyApi = useSpotify()
+  const { data: session, status } = useSession()
+  const [currentTrackId, setCurrentIdTrack] =
+    useRecoilState(currentTrackIdState)
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState)
+  const [volume, setVolume] = useState(50)
+
+  const songInfo = useSongInfo()
+
+  const fetchCurrentSong = () => {
+    if (!songInfo) {
+      spotifyApi.getMyCurrentPlayingTrack().then((data) => {
+        console.log('Now playing ', data.body?.item)
+        setCurrentIdTrack(data.body?.item?.id)
+
+        spotifyApi.getMyCurrentPlaybackState().then((data) => {
+          setIsPlaying(data.body?.is_playing)
+        })
+      })
+    }
+  }
+
+  const handlePlayPause = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((data) => {
+      if (data.body?.is_playing) {
+        spotifyApi.pause()
+        setIsPlaying(false)
+      } else {
+        spotifyApi.play()
+        setIsPlaying(true)
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken() && !currentTrackId) {
+      fetchCurrentSong()
+      setVolume(50)
+    }
+  }, [currentTrackIdState, spotifyApi, session])
+
+  useEffect(() => {
+    if (volume > 0 && volume < 100) {
+      debounceAdjustVolume(volume)
+    }
+  }, [volume])
+
+  const debounceAdjustVolume = useCallback(
+    debounce((volume) => {
+      spotifyApi.setVolume(volume).catch((err) => {})
+    }, 500),
+    []
+  )
+
+  return (
+    <div
+      className="grid h-24 grid-cols-3
+   bg-gradient-to-b from-black to-gray-900 px-2 text-xs
+    text-white md:px-8 md:text-base"
+    >
+      {/* Left */}
+      <div className="flex items-center space-x-4">
+        <img
+          className="hidden h-10 w-10 md:inline"
+          src={songInfo?.album.images?.[0]?.url}
+          alt=""
+        />
+        <div>
+          <h3>{songInfo?.name}</h3>
+          <p>{songInfo?.artists?.[0]?.name}</p>
+        </div>
+      </div>
+
+      {/*Center */}
+      <div className="flex items-center justify-evenly">
+        <SwitchHorizontalIcon className="button" />
+        <RewindIcon // onCLick={() => spotify.Api.skiptToPrevious()} --- The API is not working
+          className="button"
+        />
+
+        {isPlaying ? (
+          <PauseIcon onClick={handlePlayPause} className="button h-10 w-10" />
+        ) : (
+          <PlayIcon onClick={handlePlayPause} className="button h-10 w-10" />
+        )}
+
+        <FastForwardIcon
+          // onCLick={() => spotify.Api.skiptToNext()} --- The API is not working
+          className="button"
+        />
+        <ReplyIcon className="button" />
+      </div>
+      {/*Right */}
+      <div className="flex items-center justify-end space-x-3 pr-5 md:space-x-4">
+        <VolumeOffIcon
+          onClick={() => volume > 0 && setVolume(volume - volume)}
+          className="button"
+        />
+        <input
+          className="w-14 md:w-20"
+          type="range"
+          value={volume}
+          onChange={(e) => setVolume(Number(e.target.value))}
+          min={0}
+          max={100}
+        />
+        <VolumeUpIcon
+          onClick={() => volume < 100 && setVolume(volume + 10)}
+          className="button"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Player
